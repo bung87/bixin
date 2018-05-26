70e98d9f9d8543d77986c5bc3bac092d03acc628
zhoupeng <zhoupeng@xjgreat.com>
2018-05-27 00:24:24 +0800

    改进算法accuracy: 0.801482 下降了约0.01 速度提升了40秒，判断逻辑不再依赖posseg

--- bixin/__init__.py
+++ bixin/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import re,os
-import jieba.posseg as pseg
+# import jieba.posseg as pseg
+import jieba
 from functools import lru_cache
+import math
 import pickle 
+from collections import Counter
 
 DATA_PATH = os.path.join(os.path.dirname(__file__),"data","dict.pkl")
 
@@ -11,7 +14,7 @@ def load_data():
     print("load data")
     with open(DATA_PATH,"rb") as f:
         return pickle.load(f)
-
+FIXED_PA = 0.25
 class Classifier():
     def __init__(self,*args):
         self.initialized = False
@@ -47,24 +50,31 @@ class Classifier():
         news = re.sub(
             r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', news)
         news = re.sub("@[^\s]+", "", news)
+        news = re.sub("“[^”]+”","",news) # remove quote
+        news = re.sub("【[^】]+","",news) # remove quote
         text_len = len(news)
         if not text_len:
             return 0
-        word_list = [(x, y) for x, y in pseg.cut(news)if not re.match("\W", x)]
+        # word_list = [(x, y) for x, y in jiaba.posseg.cut(news)if not re.match("\W", x)]
+        word_list = [x  for x in jieba.cut(news)if not re.match("\W", x)]
 
         word_scored = set()
+        counter = Counter(word_list)
         # print(word_list)
         pos_dict = {'times': 0, 'score': 0, 'words': [], 'index': []}
         neg_dict = {'times': 0, 'score': 0, 'words': [], 'index': []}
 
-        for index, t in enumerate(word_list):
-            word, tag = t
-            if len(word) > 1 and tag == "n" and word in word_scored:
-                continue
+        for index, word in enumerate(word_list):
+            # word, tag = t
+            # if word in word_scored:
+            #     continue
             word_score = 0
             # 判断极性
             if (word in self.pos_emotion) or (word in self.pos_envalute):
-                word_score += 1
+                word_mark = "+" + word
+                if word_mark in word_scored:
+                    continue
+                word_score += 1 + math.log(1+counter[word])
                 '''
                 两种情况：
                 1. 非常 不 好吃
@@ -72,32 +82,37 @@ class Classifier():
                 需要极性反转
                 '''
                 if (index - 1 >= 0 and word_list[index - 1] in self.neg_degree) or (index - 2 >= 0 and word_list[index - 2] in self.neg_degree):
-                    word_score = 0.25 * (word_score + (-1))
+                    word_score = FIXED_PA * (word_score + (-1))
                 debug and print("%s pos" % word)
+                word_scored.add(word_mark)
             elif (word in self.neg_emotion) or (word in self.neg_envalute):
-                word_score -= 1
+                word_mark = "-" + word
+                if word_mark in word_scored:
+                    continue
+                word_score -= ( 1 + math.log(1+counter[word]))
                 '''
                 1. 不是 不好
                 2. 不是 很 不好
                 极性反转
                 '''
                 if (index - 1 >= 0 and word_list[index - 1] in self.neg_degree) or (index - 2 >= 0 and word_list[index - 2] in self.neg_degree):
-                    word_score = 0.25 * (word_score + (-1))
+                    word_score = FIXED_PA * (word_score + (-1))
                 debug and print("%s neg" % word)
+                word_scored.add("-"+word)
             # 判断程度词
             if index - 1 >= 0:
                 # 赫夫曼二叉树，加权路径最小
                 con = (index - 2 >= 0 and word_list[index - 2] in self.more_degree)
                 if word_list[index - 1] in self.more_degree or con:
-                    word_score = 0.25 * (word_score + 3)
+                    word_score = FIXED_PA * (word_score + 3)
                 elif word_list[index - 1] in self.ish_degree or con:
-                    word_score = 0.25 * (word_score + 2)
+                    word_score = FIXED_PA * (word_score + 2)
                 elif word_list[index - 1] in self.very_degree or con:
-                    word_score = 0.25 * (word_score + 4)
+                    word_score = FIXED_PA * (word_score + 4)
                 elif word_list[index - 1] in self.least_degree or con:
-                    word_score = 0.25 * (word_score + 1)
+                    word_score = FIXED_PA * (word_score + 1)
                 elif word_list[index - 1] in self.most_degree or con:
-                    word_score = 0.25 * (word_score + 5)
+                    word_score = FIXED_PA * (word_score + 5)
 
             if word_score > 0:
                 debug and pos_dict['words'].append(word)
@@ -109,29 +124,11 @@ class Classifier():
                 neg_dict['index'].append(index)
                 neg_dict['times'] += 1
                 neg_dict['score'] += word_score
-            word_scored.add(word)
+            
 
         debug and print(str(pos_dict)+"\n"+str(neg_dict))
 
-        pos_len = pos_dict['index']
-        neg_len = neg_dict['index']
-        pos_max = max(pos_dict['index']) if pos_len else 0
-        neg_max = max(neg_dict['index']) if neg_len else 0
-
-        pos_min = min(pos_dict['index']) if pos_len else 0
-        neg_min = min(neg_dict['index']) if neg_len else 0
-
-        pos_range = pos_max - pos_min
-        neg_range = neg_max - neg_min
-
-        pos_per = pos_range/text_len
-        neg_per = neg_range/text_len
-
-        wei = abs(pos_per-neg_per) / text_len
-        pos_wei = wei if pos_max > neg_max and pos_per > 0.1 else 0
-        neg_wei = wei if neg_max > pos_max and neg_per > 0.1 else 0
-
-        return pos_dict['score']*pos_per + pos_wei - (abs(neg_dict['score']*neg_per) + neg_wei)
+        return pos_dict['score'] - abs(neg_dict['score'])
  
 def predict(x,debug=False):
     if  predict.classifier.initialized:
