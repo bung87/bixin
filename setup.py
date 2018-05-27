e2af47148062093509f66fedbfce3e506d13388d
zhoupeng <zhoupeng@xjgreat.com>
2018-05-27 16:23:04 +0800

    o

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setup(
     name='bixin',
     version='0.0.1',
     packages=find_packages(exclude=['bin', 'tests']),
-    include_package_data = True,
-    package_data = {
+    include_package_data=True,
+    package_data={
         'bixin.data': ['*.pkl']
-     },
-    extras_require = {
-        'dev':['prefixtree>=0.2.5']
     },
-    install_requires = install_requires
+    extras_require={
+        'dev': ['prefixtree>=0.2.5', 'chardet>=3.0.4']
+    },
+    install_requires=install_requires
     # prefixtree need at least 0.2.5 if not in pypi install if from github
 )
